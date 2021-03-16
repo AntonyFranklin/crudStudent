04c8fd28fcdf3042f9b4f1044429aee65efba84b
Franklin <franklinantonylin73@gmail.com>
2021-03-16 14:10:16 +0530

    Environment Setup Completed

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import GetUser from './Components/GetUser'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GetUser />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
